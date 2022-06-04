616d335cd90f490518fa996b973f5d2104ee0608
Midul-islam-Russell <midulraddy121@gmail.com>
2022-06-05 00:40:24 +0600

    first commit

---
+++ c.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main (){
+// &  Bitwise AND
+// |  Bitwise OR
+// ^  Bitwise EXOR
+// >> Bitwise Shift Right
+// << Bitwise Shift Left
+// - Bitwise NOT
+
+int  a = 32;
+int b = 12 ;
+int c ;
+
+
+c = a & b ;
+cout<<c <<endl;
+
+c = a | b ;
+cout<<c <<endl;
+
+
+c = a ^ b ;
+cout<<c <<endl;
+
+
+
+
+
+
+getch();
+}
